590dc27fc4a5b6958976d347d877235f3abe8077
Juliengaspar <julien.gaspar@student.hepl.be>
2026-02-10 16:26:25 +0100

    added--plugin--vite

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -16,3 +16,23 @@ add_action( 'wp_enqueue_scripts', function() {
 // Remove inline global CSS on the front end.
     wp_dequeue_style( 'global-styles' );
 }, 20 );
+
+//parametre un string et en retour (:) en string
+// function regarder si une clef est bien present dans le fichier si oui ob le fais
+function dw_asset(string $file): string {
+    $manifest_path = get_theme_file_path( '/public/.vite/manifest.json' );
+    if ( file_exists( $manifest_path ) ) {
+        //retourn un tableaux asscciatif
+        $manifest = json_decode( file_get_contents( $manifest_path ), true );
+//tester si le tableaux associatif possede la valleur entrée
+        if ( isset($manifest['wp-content/themes/portfolio/assets/css/styles.scss']) && $file === 'css' ) {
+            return get_theme_file_uri( '/public/'.$manifest['wp-content/themes/portfolio/assets/css/styles.scss']['file']);
+        }
+        if ( isset($manifest['wp-content/themes/portfolio/assets/js/main.js']) && $file === 'js' ) {
+            return get_theme_file_uri( '/public/'.$manifest['wp-content/themes/portfolio/assets/js/main.js']['file']);
+
+        }
+
+    }
+    return '';
+}
